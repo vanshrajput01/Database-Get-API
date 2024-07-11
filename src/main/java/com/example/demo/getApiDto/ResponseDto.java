4927645c58e9c41df82a7d90222c80d873e57b8b
vanshrajput01 <rajputvansh009@gmail.com>
2024-07-11 13:48:00 +0530

    Entity code add

--- src/main/java/com/example/demo/getApiDto/ResponseDto.java
+++ src/main/java/com/example/demo/getApiDto/ResponseDto.java
@@ -1,5 +1,21 @@
 package com.example.demo.getApiDto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ResponseDto {
+	
+	@JsonProperty("emp_id")
+	public Long getEmp_id;
+	
+	@JsonProperty("emp_name")
+	public String getEmp_anme;
+	
+	
+	@JsonProperty("emp_salary")
+	public Long getEmp_salary;
+	
+	
+	
+	
 
 }
