43dcb64140ba308295f260fa364fa2a08ef04aae
vanshrajput01 <rajputvansh009@gmail.com>
2024-07-11 12:41:43 +0530

    get data dto code add

--- src/main/java/com/example/demo/Controller/EmpController.java
+++ src/main/java/com/example/demo/Controller/EmpController.java
@@ -1,14 +1,30 @@
 package com.example.demo.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.example.demo.Service.EmployeeService;
+import com.example.demo.getApiDto.DataDto;
+import com.example.demo.getApiDto.ResponseDto;
+
+
+
 
 
 public class EmpController {
 	
+	@Autowired
+	private EmployeeService empService;
+	
 	@GetMapping(value = "/AddEmployee",consumes = MediaType.APPLICATION_JSON_VALUE , produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> AddEmloyeeDetails(){
+	public ResponseEntity<?> AddEmloyeeDetails(@RequestBody  DataDto data){
+		ResponseDto response = this.empService.addEmployeeData(data);
+		return new ResponseEntity<>(response,HttpStatus.OK);
+		
 		
 	}
 
--- src/main/java/com/example/demo/getApiDto/DataDto.java
+++ src/main/java/com/example/demo/getApiDto/DataDto.java
@@ -1,6 +1,11 @@
 package com.example.demo.getApiDto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class DataDto {
+	
+	@JsonProperty("inputParamters")
+	public Object inputParamters;
 	
 
 }
